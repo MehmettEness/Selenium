df8f13237e4501691c7f4dbc0be868f955b3e426
Mehmet Enes <mehenar@gmail.com>
2023-11-02 11:03:54 +0300

    selenium

---
+++ src/test/java/S01_DriverFirst/C01_DriverTest.java
@@ -0,0 +1,18 @@
+package S01_DriverFirst;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class C01_DriverTest {
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+
+    }
+
+
+}
